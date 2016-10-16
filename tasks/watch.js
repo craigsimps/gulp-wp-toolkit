167446b1a7d8f2fdba88c0f257f2b75b4d16fb10
Craig Simpson <craig@craigsimpson.scot>
2016-10-16 17:05:33 +0100

    Refresh BrowserSync if something changes.

--- tasks/watch.js
+++ tasks/watch.js
@@ -9,5 +9,10 @@ module.exports = function() {
     gulp.watch(config.watch.scripts, ['build:js']);
     gulp.watch(config.watch.images, ['build:images']);
     gulp.watch(config.watch.code, ['build:i18n']);
-    gulp.watch(config.watch.root).on('change', bs.reload);
+    gulp.watch([
+        config.watch.styles,
+        config.watch.scripts,
+        config.watch.images,
+        config.watch.code
+    ]).on('change', bs.reload);
 };

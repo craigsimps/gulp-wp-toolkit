835c020e52e29459593340a89805a36f5b8d0ace
Craig Simpson <craig@craigsimpson.scot>
2016-10-16 14:30:46 +0100

    Reinstate `gulp-sequence`
    Certain tasks must only run when others are complete, such as `gulp dependencies` which needs to first install before starting to concatenate.
    
    Other tasks, such as linting, make sense to be run in series so that there is a clear definition between each tasks and preventing any output from being mixed together.

--- tasks/index.js
+++ tasks/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var sequence = require('gulp-sequence');
+
 module.exports = {
 
     'browser-sync': [require('./browser-sync')],
@@ -7,7 +9,7 @@ module.exports = {
     'dep:install': [require('./dependencies/install')],
     'dep:bowercss': [require('./dependencies/bowercss')],
     'dep:bowerjs': [require('./dependencies/bowerjs')],
-    'dependencies': [['dep:install', 'dep:bowercss', 'dep:bowerjs']],
+    'dependencies': [sequence('dep:install', ['dep:bowercss', 'dep:bowerjs'])],
 
     'build:css': [require('./build/css')],
     'build:js': [require('./build/js')],
@@ -15,7 +17,7 @@ module.exports = {
     'build:i18n': [require('./build/i18n')],
     'build:styleguide': [require('./build/styleguide')],
     'build:potomo': [require('./build/potomo')],
-    'build': [['dependencies', 'build:css', 'build:js', 'build:images', 'build:i18n']],
+    'build': [sequence('dependencies', ['build:css', 'build:js', 'build:images', 'build:i18n'])],
 
     'clean:css': [require('./clean/css')],
     'clean:js': [require('./clean/js')],
@@ -31,16 +33,16 @@ module.exports = {
     'lint:jscs': [require('./lint/jscs')],
     'lint:jsvalidate': [require('./lint/jsvalidate')],
     'lint:json': [require('./lint/json')],
-    'lint:js': [['lint:jshint', 'lint:jscs', 'lint:jsvalidate', 'lint:json']],
+    'lint:js': [sequence('lint:jshint', 'lint:jscs', 'lint:jsvalidate', 'lint:json')],
     'lint:i18n': [require('./lint/i18n')],
     'lint:colors': [require('./lint/colors')],
-    'lint': [['lint:scss', 'lint:js', 'lint:i18n', 'lint:colors']],
+    'lint': [sequence('lint:scss', 'lint:js', 'lint:i18n', 'lint:colors')],
 
     'self:jshint': [require('./self/jshint')],
     'self:jscs': [require('./self/jscs')],
     'self:jsvalidate': [require('./self/jsvalidate')],
     'self:json': [require('./self/json')],
-    'self': [['self:jshint', 'self:jscs', 'self:jsvalidate', 'self:json']],
+    'self': [sequence('self:jshint', 'self:jscs', 'self:jsvalidate', 'self:json')],
 
     'pre-commit': [['lint']],
     'bump': [require('./bump')],

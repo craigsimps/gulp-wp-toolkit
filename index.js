6d555abd95a34210b04bcd433ea4ec8ad3e38f33
Gary Jones <gamajo@gamajo.com>
2016-11-16 15:09:10 +0000

    Remove unused variables

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var merge = require('lodash.merge');
-var defaultConfig = require('./config');
-
 module.exports = {
     tasks: require('./tasks'),
     config: require('./config'),

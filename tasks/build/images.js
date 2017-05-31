39bee94554b76dbd69c3dccbd3e16587dc0c5c70
Craig Simpson <craig@craigsimpson.scot>
2017-05-31 16:10:29 +0100

    Add support for copying screenshot to theme root

--- tasks/build/images.js
+++ tasks/build/images.js
@@ -5,13 +5,19 @@ const gulp = require('gulp'),
   changed = require('gulp-changed'),
   cache = require('gulp-cache'),
   imagemin = require('gulp-imagemin'),
-  notify = require('gulp-notify');
+  notify = require('gulp-notify'),
+  tap = require('gulp-tap');
 
 module.exports = function() {
   return gulp.src(config.src.images)
     .pipe(changed(config.dest.images))
     .pipe(cache(
       imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
+    .pipe(tap(function(file) {
+      if ('screenshot.png' === path.basename(file.path)) {
+        return gulp.dest('./');
+      }
+    }))
     .pipe(gulp.dest(config.dest.images))
     .pipe(notify({message: config.messages.images}));
 };

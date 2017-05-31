6709ae6631a5b88ab60d6bf73d37dbab02fbb936
Craig Simpson <craig@craigsimpson.scot>
2017-05-31 18:07:10 +0100

    Update `build:images` task to check for screenshot, file extension and then ignore from rest of pipe

--- tasks/build/images.js
+++ tasks/build/images.js
@@ -6,18 +6,30 @@ const gulp = require('gulp'),
   cache = require('gulp-cache'),
   imagemin = require('gulp-imagemin'),
   notify = require('gulp-notify'),
-  tap = require('gulp-tap');
+  tap = require('gulp-tap'),
+  path = require('path'),
+  ignore = require('gulp-ignore');
 
 module.exports = function() {
+  let screenshotPath = false;
+
   return gulp.src(config.src.images)
     .pipe(changed(config.dest.images))
     .pipe(cache(
       imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
     .pipe(tap(function(file) {
-      if ('screenshot.png' === path.basename(file.path)) {
-        return gulp.dest('./');
+      const fullPath = file.path,
+        fileExtension = path.extname(file.path),
+        fileName = path.basename(fullPath, fileExtension),
+        allowedExtensions = ['.png', '.jpg', '.jpeg', '.gif'];
+
+      if (('screenshot' === fileName) &&
+        (-1 !== allowedExtensions.indexOf(fileExtension))) {
+        screenshotPath = fullPath;
+        return gulp.src(screenshotPath).pipe(gulp.dest('./'));
       }
     }))
+    .pipe(ignore.include(screenshotPath))
     .pipe(gulp.dest(config.dest.images))
     .pipe(notify({message: config.messages.images}));
 };

bfe4e86746877d0f4254f78d6fb4c12354ae8522
Gary Jones <gary@gamajo.com>
2017-08-23 11:43:16 +0100

    Add lint:i18n to lint:php

--- tasks/index.js
+++ tasks/index.js
@@ -32,13 +32,13 @@ module.exports = {
     'lint:eslint': [require('./lint/eslint')],
     'lint:js': [sequence('lint:jsvalidate', 'lint:json', 'lint:eslint')],
 
+    'lint:i18n': [require('./lint/i18n')],
     'lint:phpcs': [require('./lint/phpcs')],
     'lint:phpmd': [require('./lint/phpmd')],
-    'lint:php': [sequence('lint:phpcs', 'lint:phpmd')],
+    'lint:php': [sequence('lint:i18n', 'lint:phpcs', 'lint:phpmd')],
 
-    'lint:i18n': [require('./lint/i18n')],
 
-    'lint': [sequence('lint:style', 'lint:php', 'lint:js', 'lint:i18n')],
+    'lint': [sequence('lint:style', 'lint:php', 'lint:js')],
 
     'bump': [require('./bump')],
     'watch': [require('./watch')],

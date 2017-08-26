2256de9b05ccb95fe5507b5b0c0c9788873a69c1
Craig Simpson <craig@craigsimpson.scot>
2017-08-26 14:49:29 +0100

    Update branch to use `gulp-filter`. See #12

--- tasks/build/images.js
+++ tasks/build/images.js
@@ -5,13 +5,12 @@ const gulp = require('gulp'),
   changed = require('gulp-changed'),
   imagemin = require('gulp-imagemin'),
   notify = require('gulp-notify'),
-  tap = require('gulp-tap'),
-  path = require('path'),
   ignore = require('gulp-ignore'),
-  conditional = require('gulp-if');
+  filter = require('gulp-filter');
 
 module.exports = function() {
-  let screenshotPath = false;
+  const allButScreenshot = filter(['**/*', '!**/screenshot.*'], {restore: true});
+  const onlyScreenshot = filter(['**/screenshot.*']);
 
   return gulp.src(config.src.images)
     .pipe(changed(config.dest.images))
@@ -19,21 +18,12 @@ module.exports = function() {
       verbose: true,
       optimizationLevel: 3,
       progressive: true,
-      interlaced: true,
+      interlaced: true
     }))
-    .pipe(tap(function(file) {
-      const fullPath = file.path,
-        fileExtension = path.extname(file.path),
-        fileName = path.basename(fullPath, fileExtension),
-        allowedExtensions = ['.png', '.jpg', '.jpeg', '.gif'];
-
-      if (('screenshot' === fileName) &&
-        (-1 !== allowedExtensions.indexOf(fileExtension))) {
-        screenshotPath = fullPath;
-        return gulp.src(screenshotPath).pipe(gulp.dest('./'));
-      }
-    }))
-    .pipe(conditional(false !== screenshotPath, ignore.include(screenshotPath)))
+    .pipe(allButScreenshot)
     .pipe(gulp.dest(config.dest.images))
+    .pipe(allButScreenshot.restore)
+    .pipe(onlyScreenshot)
+    .pipe(gulp.dest('./'))
     .pipe(notify({message: config.messages.images}));
 };
